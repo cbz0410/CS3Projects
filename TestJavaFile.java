ee7ac8b65ae771eb9deecb3e4d48b116708b52c3
Michael Goppert <mtgoppert.04@gmail.com>
2022-08-10 09:11:25 -0500

    Insert Test/Template File

---
+++ TestJavaFile.java
@@ -0,0 +1,13 @@
+/* Goopert
+ * 1A Malloy
+ * - insert date - 
+ * Program Discrioption: - put a mundane description here -
+ */
+
+public class TestJavaFile
+{
+    public static void main(String[] args)
+    {
+        //> insert code here
+    }
+}
