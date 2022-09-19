fc042316686729a9691dbcbae773acc8ffd15dc1
Michael Goppert <mtgoppert.04@gmail.com>
2022-09-19 13:29:22 -0500

    Upload Chapter 23 & 24

---
+++ Notes/Chapter 23 & 24/DotProductCalculator.java
@@ -0,0 +1,80 @@
+/* Goopert
+ * 1A Malloy
+ * 9/19/22
+ * Program Discrioption: Uses a scanner and calculate the dot product of two vectors, unlimited in dimension 
+ *                   IE: a • b = <a_1, a_2, ..., a_n> • <b_1, b_2, ..., b_n> 
+ *                             = (a_1 * b_1) + (a_2 * b_2) + ... + (a_n * b_n) 
+ * 
+ * Example Formatting for the Input File:
+ * 
+ * 7 13 21
+ * 64 37 82
+ * 
+ * Expected Example Result:
+ *
+ * 2651.0
+ * 
+ */
+
+import java.io.*;
+import java.util.*;
+
+//> NOTE: This program only works for two vectors (ex. finding a • b)
+public class DotProductCalculator 
+{
+    public static void main(String[] args) throws IOException
+    {
+        Scanner fileReader = new Scanner(new File("vectors.in"));
+        ArrayList<Vector> queue = new ArrayList<Vector>(); //> list of vectors being processed
+
+        //> loop controlling the whol vector being processed
+        while(fileReader.hasNextLine())
+        {
+            Scanner vectorReader = new Scanner(fileReader.nextLine());
+            ArrayList<Double> vectorElements = new ArrayList<Double>(); //> current vector elements
+            
+            //> loop controlling the elements of the vector bieng processed
+            while(vectorReader.hasNextDouble())
+            {
+                vectorElements.add(vectorReader.nextDouble());
+            }
+
+            queue.add(new Vector(vectorElements));
+
+            vectorReader.close();
+        }
+
+        fileReader.close();
+
+        System.out.println(queue.get(0).calculateDotProduct(queue.get(1)));
+    }
+}
+
+class Vector
+{
+    private ArrayList<Double> elements;
+
+    public Vector(ArrayList<Double> elements)
+    {
+        this.elements = elements;
+    }
+
+    //> returns a • b if possible, otherwise returns -1
+    public double calculateDotProduct(Vector b)
+    {
+        if(elements.size() == b.elements.size())
+        {
+            double result = 0;
+
+            //> calculates any finite number of elements, n
+            for(int i = 0; i < elements.size(); i++)
+            {
+                result += elements.get(i) * b.elements.get(i);
+            }
+
+            return result;
+        }
+
+        return -1;
+    }
+}
---
+++ Notes/Chapter 23 & 24/FileInputPractice.java
@@ -0,0 +1,65 @@
+/* Goopert
+ * 1A Malloy
+ * 9/13/22
+ * Program Discrioption: Class practice reading input from a file */
+
+import java.io.*;
+import java.util.*;
+
+public class FileInputPractice
+{
+    //> IOException is a safeguard, prevents errors from input
+    public static void main(String[] args) throws IOException
+    {
+        Scanner fileReader = new Scanner(new File("data.in"));
+
+        Student[] roster = new Student[fileReader.nextInt()];
+        int gradesPerStudent = fileReader.nextInt();
+        fileReader.nextLine();
+
+        for(int i = 0; i < roster.length; i++)
+        {
+            Scanner lineReader = new Scanner(fileReader.nextLine());
+            String tempName = lineReader.next();
+            double[] tempGrades = new double[gradesPerStudent];
+
+            for(int u = 0; u < gradesPerStudent; u++)
+            {
+                tempGrades[u] = lineReader.nextDouble();
+            }
+
+            roster[i] = new Student(tempName, tempGrades);
+        }
+
+        fileReader.close();
+
+        for(Student student : roster)
+        {
+            System.out.println(student.name + ": " + student.findAverage());
+        }
+    }
+}
+
+class Student
+{
+    String name;
+    double[] grades;
+    
+    public Student(String name, double[] grades)
+    {
+        this.name = name;
+        this.grades = grades;        
+    }
+
+    public double findAverage()
+    {
+        double sum = 0;
+
+        for(int i = 0; i < grades.length; i++)
+        {
+            sum += grades[i];
+        }
+
+        return sum / grades.length;
+    }
+}
